c5956fdbd747254dfb2a1d9583fa618b04153ba5
Zsolt <184605676+Zsolt-qwerty@users.noreply.github.com>
2024-12-05 15:45:24 +0000

    readability and UI polishing

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ function compare(playerMove, computerMove) {
   if (playerMove === computerMove) {
     return 0;
   } else if (
-    (playerMove === "rock" && computerMove === "scissors") ||
+    (playerMove === "rock"     && computerMove === "scissors") ||
     (playerMove === "scissors" && computerMove === "paper") ||
-    (playerMove === "paper" && computerMove === "rock")
+    (playerMove === "paper"    && computerMove === "rock")
   ) {
     return 1;
   } else {
@@ -19,25 +19,22 @@ function getComputerMove() {
 }
 
 let playerName;
-
 let isNameCorrect = false;
-  while (isNameCorrect === false) {
-     playerName = prompt("What is your name");
-    
-    if (playerName.length <= 10) {
-      isNameCorrect = true;
-    } else {
-      alert("Name is too long, must be no more than 10 characters!");
-    }
+
+while (isNameCorrect === false) {
+  playerName = prompt("What is your name?");
+  if (playerName.length <= 10) {
+    isNameCorrect = true;
+  } else {
+    alert("Name is too long, must be no more than 10 characters!");
   }
+}
   
-  
-  
-  let scores = {
-    gamesPlayed: 0,
-    win: 0,
+let scores = {
+  gamesPlayed: 0,
+  win:  0,
   lose: 0,
-  draw: 0
+  draw: 0,
 }
 
 alert (`Hello ${playerName}, wanna play?`);
@@ -48,25 +45,21 @@ while (playAgain === true) {
   const playerMove = prompt("Make your move - rock, paper or scissors:");
   const computerMove = getComputerMove();
   const result = compare(playerMove, computerMove);
- 
-scores.gamesPlayed++
 
- if (result === 1) {
-  scores.win++;
-alert("YOU WON!");  
-} 
+  scores.gamesPlayed++;
+  if (result === 1) {
+    scores.win++;
+    alert("YOU WON!");  
+  } else if (result === -1) {
+    scores.lose++;
+    alert("YOU LOSE!");
+  } else { 
+    scores.draw++;
+    alert("IT WAS A DRAW!");
+  }
 
- else if (result === -1) { scores.lose++;
- alert("YOU LOSE!");
- }
- else { 
-  scores.draw++;
-  alert("IT WAS A DRAW!");
- }
- alert(
-    `${playerName}: ${playerMove}, Computer chose: ${computerMove} Wins: ${scores.win}, Losses: ${scores.lose}, Draws: ${scores.draw}. Games Played: ${scores.gamesPlayed}`
-  );
+  alert(`${playerName}: ${playerMove}\nComputer: ${computerMove}\nWins: ${scores.win}\nLosses: ${scores.lose}\nDraws: ${scores.draw}\nGames Played: ${scores.gamesPlayed}`);
   playAgain = confirm("Would you like to play again?");
 }
 
-alert(`Game over!`);
+alert(`Game over!\nGood bye!`);
